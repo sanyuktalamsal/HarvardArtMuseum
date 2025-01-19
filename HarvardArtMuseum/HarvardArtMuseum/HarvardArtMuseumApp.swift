a97980fb6511e8b56dafd42387db1452119845a7
sanyuktalamsal <112648817+sanyuktalamsal@users.noreply.github.com>
2025-01-19 15:35:03 -0500

    Made project

---
+++ HarvardArtMuseum/HarvardArtMuseum/HarvardArtMuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarvardArtMuseumApp.swift
+//  HarvardArtMuseum
+//
+//  Created by Sanyukta Lamsal on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HarvardArtMuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HarvardArtMuseum/HarvardArtMuseumTests/HarvardArtMuseumTests.swift
@@ -0,0 +1,16 @@
+//
+//  HarvardArtMuseumTests.swift
+//  HarvardArtMuseumTests
+//
+//  Created by Sanyukta Lamsal on 1/19/25.
+//
+
+import Testing
+
+struct HarvardArtMuseumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
